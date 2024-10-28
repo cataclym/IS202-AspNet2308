015c12dd44c789d762c191ad11a32f90516a9b80
jesperhh01 <jesp01@outlook.com>
2024-10-28 16:06:31 +0100

    Gjorde string coordinates nullable

--- Models/ReportViewModel.cs
+++ Models/ReportViewModel.cs
@@ -9,6 +9,9 @@ public class ReportViewModel
 {
     public int ReportId { get; set; }
     public int UserId { get; set; }
+    public string? Coordinates { get; set; }
+
+    
 
     [Required]
     [MinLength(5, ErrorMessage = "Meldingen må være minst 5 tegn lang.")]
@@ -18,11 +21,11 @@ public class ReportViewModel
     [Required(ErrorMessage = "Du må markere området på kartet.")] 
     public string GeoJsonString { get; set; }
     
-    public string Coordinates { get; set; }
     
     public DateTime CreatedAt { get; set; }
     public DateTime? ResolvedAt { get; set; }
     public Status Status { get; set; }
+
 
     public string StringKoordinaterLag
     {
