9d4f26e62ff62af637c7331746139ce0a9e56bfe
Cata <cata@cata.moe>
2024-11-11 16:03:04 +0100

    Hovedrefactorering

--- Kartverket/Services/UserService.cs
+++ Kartverket/Services/UserService.cs
@@ -1,14 +1,7 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Kartverket.Database;
 using Kartverket.Database.Models;
-using Kartverket.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-
 
 namespace Kartverket.Services;
 
