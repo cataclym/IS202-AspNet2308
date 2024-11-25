e114424fd2e5432d22e2e29eb1b50ff29f88d4e9
Cata <cata@cata.moe>
2024-11-25 12:34:48 +0100

    Tester i alle kontrollene

--- Kartverket.Tests/Controllers/ReportsControllerUnitTests.cs
+++ Kartverket.Tests/Controllers/ReportsControllerUnitTests.cs
@@ -1,6 +1,40 @@
+using Kartverket.Controllers;
+using Kartverket.Database;
+using Kartverket.Models;
+using Kartverket.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
 namespace Kartverket.Tests.Controllers;
 
 public class ReportsControllerUnitTests
 {
-    
+    private ILogger<ReportsController> _logger;
+    private IMunicipalityService _municipalityService;
+    private GeoJsonService _geoJsonService;
+    private IUserService _userService;
+    private ApplicationDbContext _applicationDbContext;
+
+    private ReportsController GetUnitUnderTest()
+    {
+        // Returner homecontroller med substituerte parametere i konstruktøren
+        _logger = Substitute.For<ILogger<ReportsController>>();
+        _municipalityService = Substitute.For<IMunicipalityService>();
+        _userService = Substitute.For<IUserService>();
+        
+        var homeController = new ReportsController(null, _logger, _municipalityService, null, _userService);
+        homeController.ControllerContext.HttpContext = new DefaultHttpContext();
+        return homeController;
+    }
+
+    [Fact]
+    public void UsingMapReportReturnsNullModel()
+    {
+        ReportsController unit = GetUnitUnderTest();
+        var result = unit.MapReport() as ViewResult;
+        var model = result.Model as ReportViewModel;
+        Assert.Null(model);
+    }
 }
